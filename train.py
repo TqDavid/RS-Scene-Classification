00ba28c84ea0018f9d4922a1218bcf7b71fc451b
Nick Nie <50539160+niecongchong@users.noreply.github.com>
2019-05-28 21:30:10 +0800

    update

--- train.py
+++ train.py
@@ -1,173 +1,82 @@
-from batch_generater import load_data_path, batch_generator
+from keras.applications.resnet50 import ResNet50
 from keras.models import Model
-from keras.layers import Input, Dense, Dropout, BatchNormalization, Conv2D, MaxPooling2D, AveragePooling2D, concatenate, \
-    Activation, ZeroPadding2D
-from keras.layers import add, Flatten
-from keras.metrics import top_k_categorical_accuracy
-from keras.optimizers import SGD
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout
 from keras.callbacks import ModelCheckpoint
-import matplotlib.pyplot as plt
+from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
-from imutils import paths
-# from tensorflow.keras.callbacks import TensorBoard
-from params import *
+from keras.metrics import top_k_categorical_accuracy
+import matplotlib.pyplot as plt
 
 
+# ---------------------网络结构搭建------------------------- #
+# 此处直接调用keras_application中定义好的网络结构，去除全连接层
+base_model = ResNet50(weights='imagenet', include_top=False)
 
-def Conv2d_BN(x, nb_filter, kernel_size, strides=(1, 1), padding='same'):
-    x = Conv2D(nb_filter, kernel_size, padding=padding, strides=strides, activation='relu')(x)
-    x = BatchNormalization(axis=3)(x)
-    return x
-
-
-def identity_Block(inpt, nb_filter, kernel_size, strides=(1, 1), with_conv_shortcut=False):
-    x = Conv2d_BN(inpt, nb_filter=nb_filter, kernel_size=kernel_size, strides=strides, padding='same')
-    x = Conv2d_BN(x, nb_filter=nb_filter, kernel_size=kernel_size, padding='same')
-    if with_conv_shortcut:
-        shortcut = Conv2d_BN(inpt, nb_filter=nb_filter, strides=strides, kernel_size=kernel_size)
-        x = add([x, shortcut])
-        return x
-    else:
-        x = add([x, inpt])
-        return x
+# 搭建全连接层，添加Dropout防止过拟合
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation='relu', name='fc1')(x)
+x = Dropout(0.5)(x)
+x = Dense(512, activation='relu', name='fc2')(x)
+x = Dropout(0.5)(x)
+x = Dense(45, activation='softmax', name='predictions')(x)
+model = Model(input=base_model.input, output=x)
+model.summary()
 
 
-def bottleneck_Block(inpt,nb_filters,strides=(1,1),with_conv_shortcut=False):
-    k1, k2, k3=nb_filters
-    x = Conv2d_BN(inpt, nb_filter=k1, kernel_size=1, strides=strides, padding='same')
-    x = Conv2d_BN(x, nb_filter=k2, kernel_size=3, padding='same')
-    x = Conv2d_BN(x, nb_filter=k3, kernel_size=1, padding='same')
-    if with_conv_shortcut:
-        shortcut = Conv2d_BN(inpt, nb_filter=k3, strides=strides, kernel_size=1)
-        x = add([x, shortcut])
-        return x
-    else:
-        x = add([x, inpt])
-        return x
+# 超参数
+PATH = '/content/NWPU-RESISC45/'
+BATCH_SIZE = 16
+TEST_SIZE = 0.3
+EPOCHS = 100
+CLASSES = 21
 
 
-def resnet_34(width,height,channel,classes):
-    inpt = Input(shape=(width, height, channel))
-    x = ZeroPadding2D((3, 3))(inpt)
-
-    #conv1
-    x = Conv2d_BN(x, nb_filter=64, kernel_size=(7, 7), strides=(2, 2), padding='valid')
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
-
-    #conv2_x
-    x = identity_Block(x, nb_filter=64, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=64, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=64, kernel_size=(3, 3))
-
-    #conv3_x
-    x = identity_Block(x, nb_filter=128, kernel_size=(3, 3), strides=(2, 2), with_conv_shortcut=True)
-    x = identity_Block(x, nb_filter=128, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=128, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=128, kernel_size=(3, 3))
-
-    #conv4_x
-    x = identity_Block(x, nb_filter=256, kernel_size=(3, 3), strides=(2, 2), with_conv_shortcut=True)
-    x = identity_Block(x, nb_filter=256, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=256, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=256, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=256, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=256, kernel_size=(3, 3))
-
-    #conv5_x
-    x = identity_Block(x, nb_filter=512, kernel_size=(3, 3), strides=(2, 2), with_conv_shortcut=True)
-    x = identity_Block(x, nb_filter=512, kernel_size=(3, 3))
-    x = identity_Block(x, nb_filter=512, kernel_size=(3, 3))
-    x = AveragePooling2D(pool_size=(7, 7))(x)
-    x = Flatten()(x)
-    x = Dense(classes, activation='softmax')(x)
-
-    model = Model(inputs=inpt, outputs=x)
-    return model
-
-
-def resnet_50(width, height, channel, classes):
-    inpt = Input(shape=(width, height, channel))
-    x = ZeroPadding2D((3, 3))(inpt)
-    x = Conv2d_BN(x, nb_filter=64, kernel_size=(7, 7), strides=(2, 2), padding='valid')
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
-
-    #conv2_x
-    x = bottleneck_Block(x, nb_filters=[64,64,256],strides=(1,1),with_conv_shortcut=True)
-    x = bottleneck_Block(x, nb_filters=[64,64,256])
-    x = bottleneck_Block(x, nb_filters=[64,64,256])
-    x = Dropout(0.5)(x)
-
-    #conv3_x
-    x = bottleneck_Block(x, nb_filters=[128, 128, 512],strides=(2,2),with_conv_shortcut=True)
-    x = bottleneck_Block(x, nb_filters=[128, 128, 512])
-    x = bottleneck_Block(x, nb_filters=[128, 128, 512])
-    x = bottleneck_Block(x, nb_filters=[128, 128, 512])
-    x = Dropout(0.5)(x)
-
-    #conv4_x
-    x = bottleneck_Block(x, nb_filters=[256, 256, 1024],strides=(2,2),with_conv_shortcut=True)
-    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-    x = Dropout(0.5)(x)
-
-    #conv5_x
-    x = bottleneck_Block(x, nb_filters=[512, 512, 2048], strides=(2, 2), with_conv_shortcut=True)
-    x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
-    x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
-    x = Dropout(0.5)(x)
-
-    x = AveragePooling2D(pool_size=(7, 7))(x)
-    x = Flatten()(x)
-    x = Dropout(0.5)(x)
-    x = Dense(classes, activation='softmax')(x)
-
-    model = Model(inputs=inpt, outputs=x)
-    return model
-
-
-# 读取数据
-datagen = ImageDataGenerator(rotation_range=10,
+# -----------------读取数据（参考kers文档理解以下函数的作用）------------------ #
+# 数据扩充
+datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                             rotation_range=10,
                              horizontal_flip=True,
-                             rescale=1./255,
                              validation_split=TEST_SIZE)
-train_batches = datagen.flow_from_directory(PATH, target_size=(256, 256), classes=None, batch_size=BATCH_SIZE)
+# 训练数据
+train_generator = datagen.flow_from_directory(PATH, target_size=(256, 256),
+                                            classes=None, batch_size=BATCH_SIZE,
+                                            subset='training')
+# 验证数据
+validation_generator = datagen.flow_from_directory(PATH, target_size=(256, 256),
+                                            classes=None, batch_size=BATCH_SIZE,
+                                            subset='validation')
 
 
-model = resnet_50(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL, NUM_CLASSES)
+# -----------------------------------模型训练-------------------------------------- #
+# 模型编译
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=[top_k_categorical_accuracy, 'acc'])
+# 模型断点保存
+model_checkpoint = ModelCheckpoint('vgg16.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
 
-# sgd = SGD(lr=1e-2, decay=1e-5, momentum=0.9, nesterov=True)
-model.compile(optimizer=OPTIMIZER, loss=LOSS, metrics=MERTICS)
-
-model_checkpoint = ModelCheckpoint('resnet50.hdf5', monitor='loss', verbose=1, save_best_only=True)
-
-imagePaths = sorted(list(paths.list_images(PATH)))
-img_numbers = len(imagePaths)
-test_img_numbers = int(img_numbers * TEST_SIZE)
-train_img_numbers = img_numbers - test_img_numbers
-
-result = model.fit_generator(generator=train_batches,
-                             steps_per_epoch=train_img_numbers // BATCH_SIZE,
+# train
+result = model.fit_generator(generator=train_generator,
+                             steps_per_epoch=train_generator.samples // BATCH_SIZE,
                              epochs=EPOCHS,
                              verbose=1,
                              callbacks=[model_checkpoint],
-                             validation_steps=test_img_numbers // BATCH_SIZE,
+                             validation_data=validation_generator,
+                             validation_steps=validation_generator.samples // BATCH_SIZE,
                              shuffle=True)
 
-plt.figure
+
+# -----------------------------------可视化模型训练曲线------------------------------- #
+plt.figure()
 plt.plot(result.epoch, result.history['acc'], label="acc")
 plt.plot(result.epoch, result.history['val_acc'], label="val_acc")
 plt.scatter(result.epoch, result.history['acc'], marker='*')
 plt.scatter(result.epoch, result.history['val_acc'])
 plt.legend(loc='under right')
 plt.show()
-plt.figure
+plt.figure()
 plt.plot(result.epoch, result.history['loss'], label="loss")
 plt.plot(result.epoch, result.history['val_loss'], label="val_loss")
 plt.scatter(result.epoch, result.history['loss'], marker='*')
-plt.scatter(result.epoch, result.history['val_loss'], marker='*')
+plt.scatter(result.epoch, result.history['val_loss'])
 plt.legend(loc='upper right')
 plt.show()
-
